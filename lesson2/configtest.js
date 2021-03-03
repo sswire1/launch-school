69e248a715e519cd4143ec213c1f4f4d4b7ce148
sswire1 <samuelswire1@gmail.com>
2021-03-03 15:55:39 -0500

    first commit

---
+++ lesson2/configtest.js
@@ -0,0 +1,45 @@
+const config = {
+  "chooseLanguagePrompt" : "1) English (en); 2) 中文 (zh); 3) Español (es)?", 
+  "promptLanguage":{
+    "en" :
+    {
+      "openingPrompt" : "Welcome to Calculator!",
+      "beginCalculatingPrompt" : "Would you like to crunch some numbers? (y/n)",
+      "validateYNPrompt" : "Please answer using 'y' for 'Yes' or 'n' for 'No'",
+      "firstNumberPrompt" : "What is your first number?",
+      "secondNumberPrompt" : "What is your second number?",
+      "invalidNumberPrompt": "Hmmm that doesn't look like a valid number...try again",
+      "invalidOperationPrompt" : "Hmmm that doesn't look like a valid number...please choose a number between 1-4",
+      "operationChoicePrompt" : "What operation would you like to perform? \n 1) Addition; 2) Subtraction; 3) Multiplication; 4) Division",
+      "calculateMorePrompt" :"Would you like to calculator some more?",
+      "terminationPrompt" : "Thanks for calculating with me!"
+    },
+  "zh" :
+    {
+      "openingPrompt" : "欢迎使用计算器" ,
+      "beginCalculatingPrompt" : "您想计算一些数字吗？（是/否）",
+      "validateYNPrompt": "请回答使用'y'代表'是'或'n'代表'否'",
+      "firstNumberPrompt" :"您的第一个电话号码是什么？",
+      "secondNumberPrompt": "您的第二个电话号码是什么？",
+      "invalidNumberPrompt" : "嗯,您输入的号码似乎无效...请重试",
+      "invalidOperationPrompt" : "嗯,看起来好像不是有效的数字...请选择一个介于1-4之间的数字",
+      "operationChoicePrompt" :"您想执行什么操作？1）加法； 2）减法； 3）乘法； 4）除法",
+      "calculateMorePrompt" :"您想再计算一些吗",
+      "terminationPrompt": "谢谢你和我一起计算"
+    },
+    "es" :
+    {
+      "openingPrompt" : "¡Bienvenido a Calculadora!",
+      "beginCalculatingPrompt" : "¿Le gustaría hacer algunos números? (y / n)",
+      "validateYNPrompt" : "Por favor responda usando 'y' para 'Sí' o 'n' para 'No'",
+      "firstNumberPrompt" : "¿Cuál es tu primer número?",
+      "secondNumberPromppt" : "¿Cuál es tu segundo número?",
+      "invalidNumberPrompt" : "Mmmm, eso no parece un número válido ... inténtalo de nuevo",
+      "invalidOperationPrompt" : "Mmmm, eso no parece un número válido ... elige un número entre 1 y 4",
+      "operationChoicePrompt" : "¿Qué operación le gustaría realizar? 1) Suma; 2) Resta; 3) Multiplicación; 4) División",
+      "calculateMorePrompt" : "¿Le gustaría calculadora un poco más?",
+      "terminationPrompt" : "¡Gracias por calcular conmigo!"
+    }
+  }
+}
+
